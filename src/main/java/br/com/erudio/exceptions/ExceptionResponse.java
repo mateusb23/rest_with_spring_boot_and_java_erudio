fc24935cc19bbc4b8a2e6bb4d6278330981ab7b9
mateusb23 <mateusb.bispo@gmail.com>
2023-01-07 18:58:03 -0300

    Project update

---
+++ src/main/java/br/com/erudio/exceptions/ExceptionResponse.java
@@ -0,0 +1,11 @@
+package br.com.erudio.exceptions;
+
+import java.io.Serializable;
+
+public class ExceptionResponse implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+}
