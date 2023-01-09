575a00a86f7c59528502b129479c1b1a27da5ba6
mateusb23 <mateusb.bispo@gmail.com>
2023-01-09 15:26:41 -0300

    Project update

--- src/main/java/br/com/erudio/controllers/PersonController.java
+++ src/main/java/br/com/erudio/controllers/PersonController.java
@@ -3,19 +3,24 @@ package br.com.erudio.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.erudio.model.Person;
 import br.com.erudio.services.PersonServices;
 
 @RestController
+@RequestMapping(value = "/person")
 public class PersonController {
 
 	@Autowired
@@ -27,7 +32,7 @@ public class PersonController {
 	}
 	
 	@GetMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value = "id") String id) {
+	public Person findById(@PathVariable(value = "id") Long id) {
 		return personServices.findById(id);
 	}
 	
@@ -37,15 +42,17 @@ public class PersonController {
 		return personServices.create(person);
 	}
 	
-	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+	@PutMapping(value = "/{id}",
+				consumes = MediaType.APPLICATION_JSON_VALUE,
 				produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person update(@RequestBody Person person) {
+	public Person update(@PathVariable(value = "id") Long id, @RequestBody Person person) {
 		return personServices.update(person);
 	}
 	
 	@DeleteMapping(value = "/{id}")
-	public void delete(@PathVariable(value = "id") String id) {
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
 		personServices.delete(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 
 
--- src/main/java/br/com/erudio/exceptions/ResourceNotFoundException.java
+++ src/main/java/br/com/erudio/exceptions/ResourceNotFoundException.java
@@ -3,11 +3,13 @@ package br.com.erudio.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+	
 	public ResourceNotFoundException(String ex) {
 		super(ex);
 	}
 
-	private static final long serialVersionUID = 1L; }
+ }
