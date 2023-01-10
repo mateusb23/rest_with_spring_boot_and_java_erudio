e09c72a644926f76d792ef7ada6a3f8ac348388b
mateusb23 <mateusb.bispo@gmail.com>
2023-01-10 12:53:36 -0300

    Implementing content negotiation by header parameter

--- src/main/java/br/com/erudio/config/WebConfig.java
+++ src/main/java/br/com/erudio/config/WebConfig.java
@@ -11,12 +11,24 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 		
+		// VIA QUERY PARAMETER http://localhost:8080/api/person/v1?mediaType=xml
+		/*
 		configurer.favorParameter(true)
 		.parameterName("mediaType").ignoreAcceptHeader(true)
 		.useRegisteredExtensionsOnly(false)
 		.defaultContentType(MediaType.APPLICATION_JSON)
 			.mediaType("json", MediaType.APPLICATION_JSON)
 			.mediaType("xml", MediaType.APPLICATION_XML);
-	}
+		*/
+		
+		// VIA HEADER PARAMETER http://localhost:8080/api/person/v1
+		configurer.favorParameter(false)
+		.ignoreAcceptHeader(false)
+		.useRegisteredExtensionsOnly(false)
+		.defaultContentType(MediaType.APPLICATION_JSON)
+			.mediaType("json", MediaType.APPLICATION_JSON)
+			.mediaType("xml", MediaType.APPLICATION_XML);
+		
+	} 
 
 }
